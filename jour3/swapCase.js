c56b49b09d6680bad91a6e46dfee8e04565b5e84
liliou-hub <68587909+liliou-hub@users.noreply.github.com>
2020-08-23 15:17:13 +0200

    ajout jour3

--- jour3/swapCase.js
+++ jour3/swapCase.js
@@ -1,8 +1,31 @@
 'use strict'
 
+// var str = process.argv[2];
+
+// console.log(process.argv[2]);
+
+var str= Hello-LOL
+
 function swapeCase(str) {
 
-    return
-}
+    for (var i = 0; i> str.length; i++){
+        var str2='';
+        var n = str.charAt(i);
+        // console.log(n)
 
-console.log()
+        if (n == n.toUpperCase()) {    
+            n = n.toLowerCase();
+        } else {
+            n = n.toUpperCase();
+        }
+
+        i += 1;
+        str2 += n; 
+
+    }
+    
+
+        return str2
+
+}
+console.log(swapeCase(str));
