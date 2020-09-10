da7de74c033f59c9c42a420a9615c7a21ddfe94e
liliou-hub <68587909+liliou-hub@users.noreply.github.com>
2020-09-10 16:52:26 +0200

    moreOrLess

--- jour5/moreOrLess/index.js
+++ jour5/moreOrLess/index.js
@@ -1,18 +1,50 @@
-var prompt = require(' prompt ');
+'use strict'
 
-//
-//  Démarre l'invite
-//
-invite.start();
+//1. Faire une fonction random qui prends un nombre entier de 0 et 100.
+//2.stocker la fonction dans une variable
+// 3. intégrer la variable dans prompt
+// 4. créer 3 conditions quand le chiffre saisi par l'utilisateur est soit  > au random, soit < au random soit === random
+// QUESTION: lorsque result.n===num il ne prend pas en copmpte l'instruction
 
-//
-//  Obtient deux propriétés de l'utilisateur: nom d'utilisateur et email
-//
-invite.get([' username ', ' email '], function (err, result) {
-  //
-  //  Enregistre les résultats.
-  //
-  console.log(' Entrée de ligne de commande reçue: ');
-  console.log('   nom d' utilisateur: ' + résultat . nom d'utilisateur);
-  console.log('   email:  ' + résultat.email);
-});
+
+function randomNumber(max) { 
+  return Math.floor(Math.random() * Math.floor(max));
+}
+console.log(randomNumber(100));
+
+var num = randomNumber(100);
+
+var prompt = require('prompt');
+
+prompt.start();
+
+function displayPrompt() {
+
+  var num = randomNumber(100);
+
+
+  prompt.get({ name: 'n', description: 'Quel est le nombre ?' }, function (
+    err,
+    result
+   
+  ) {
+    if (result.n == num) {
+      console.log('Bravo, vous avez trouve le nombre mystère !!!') ;  
+    } else if (result.n > num) {
+      console.log('cest moins!');
+      displayPrompt()
+    } else if (result.n < num) {
+      console.log('cest plus!')
+      displayPrompt()
+      ;
+    }
+    console.log(result)
+    console.log(result.n)
+  });
+}
+
+
+
+displayPrompt();
+
+
