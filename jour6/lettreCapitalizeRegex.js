014c5b90010f39fa4f65236b8369d3eb26547ac0
liliou-hub <68587909+liliou-hub@users.noreply.github.com>
2020-09-11 16:55:31 +0200

    lettercapitalizeregex

--- jour6/lettreCapitalizeRegex.js
+++ jour6/lettreCapitalizeRegex.js
@@ -1,16 +1,26 @@
 'use strict'
 
-function letterCapitalizeRegex() {
-
+// 1. je parcours la string avec "match" pour récupérer les premiers caractères de chaque mot grace au pattern
+// 2. je tranforme chaque premier caractère isolé en majuscule grâce à UpperToCase  
+// 3. je recrée chaque mot de la string grâce à "join" ou à la concaténation
+​
+// OU
+​
+// 1. je parcours la string avec "replace" pour remplacer tous les premiers caractères par leur majuscule
+​
+​
+​
+​
+// function letterCapitalizeRegex(str) {
     var str = "what a wonderful world";
+    //   console.log(word);
+    var word = str.replace(/\b[a-z]/g, function(param){
+        return (/\b[A-Z]/g)
 
-    var word = str.match(/\b[a-z]/g,);
+    });
+​
+   
+​
 
-    return word;
-}
-
-var newWord = letterCapitalizeRegex(toUpperCase())
-
-console.log(newWord);
-
-
+​
+console.log(word)
